4bdf96a0900d912b044d29865e08159b68a61ce1
Jonas Jansen <jonas.jansen98@web.de>
2020-12-16 11:47:08 +0100

    solved day 16 puzzle 2 (refactoring necessary!)

--- src/day16/puzzle.c
+++ src/day16/puzzle.c
@@ -25,6 +25,16 @@ TicketField* parseTicketFields(char* input) {
     return ticketField;
 }
 
+int isWithinFieldRanges(int value, TicketField* field) {
+    //printf("check if %i is in (%i-%i) or (%i-%i)", value, field->ranges[0][0], field->ranges[0][1], field->ranges[1][0], field->ranges[1][1]);
+    if((value >= field->ranges[0][0] && value <= field->ranges[0][1]) || (value >= field->ranges[1][0] && value <= field->ranges[1][1])) {
+        //printf("--> true\n");
+        return 1;
+    }
+    //printf("--> false\n");
+    return 0;
+}
+
 static int comp(const void* p1, const void* p2) {
   int* arr1 = (int*)p1;
   int* arr2 = (int*)p2;
@@ -87,22 +97,90 @@ int main(int argc, char *argv[]) {
 
     int scanningErrorRate = 0;
 
+    int fieldToPosMatrix[NUM_TICKET_FIELDS][NUM_TICKET_FIELDS];
+    for(int i = 0; i < NUM_TICKET_FIELDS; i++) {
+        for(int j = 0; j < NUM_TICKET_FIELDS; j++) {
+            fieldToPosMatrix[i][j] = 1;
+        }
+    }
+
     for(int i = NEARBY_TICKETS_START_LINE; i < file.lineCount; i++) {
         char* valueString = strtok(file.lines[i], ",\n");
         int idx = 0;
+        int valid = 1;
+        int ticket[NUM_TICKET_FIELDS];
         while(valueString != NULL) {
             int value = atoi(valueString);
+            ticket[idx] = value;
+            idx++; 
+
             for(int j = 0; j < numUnionizedRanges; j++) {
                 if(value >= unionizedRanges[j][0] && value <= unionizedRanges[j][1]) {
                     break;
                 }
                 scanningErrorRate += value;
+                valid = 0;
             }
-            idx++; 
             valueString = strtok(NULL, ",\n");
+        }
+
+        if(valid) {
+            for(int availableFieldsIdx = 0; availableFieldsIdx < NUM_TICKET_FIELDS; availableFieldsIdx++) {
+                for(int currTicketFieldIdx = 0; currTicketFieldIdx < NUM_TICKET_FIELDS; currTicketFieldIdx++) {
+                    if(!isWithinFieldRanges(ticket[currTicketFieldIdx], fields[availableFieldsIdx])) {
+                        fieldToPosMatrix[availableFieldsIdx][currTicketFieldIdx] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    int positionsInTicket [NUM_TICKET_FIELDS];
+    for(int i = 0; i < NUM_TICKET_FIELDS; i++) {
+        positionsInTicket[i] = -1;
+    }
+
+    for(int i = 0; i < NUM_TICKET_FIELDS; i++) {
+        int possibilitiesForField = 0;
+        int posLastPossibility = -1;
+        for(int j = 0; j < NUM_TICKET_FIELDS; j++) {
+            if(fieldToPosMatrix[i][j] == 1) {
+                 possibilitiesForField += 1;
+                 posLastPossibility = j;
+            }
+        }
+        if(possibilitiesForField == 1 && positionsInTicket[i] == -1) {
+
+            positionsInTicket[i] = posLastPossibility;
+            for(int k = 0; k < NUM_TICKET_FIELDS; k++) {
+                if(k != i) {
+                    fieldToPosMatrix[k][posLastPossibility] = 0;
+                }
+            }
+            i = -1;
+        }
+    }
+
+    int myTicket[NUM_TICKET_FIELDS];
+    int idx = 0;
+
+    char* myTicketValueString = strtok(file.lines[MY_TICKET_LINE], ",\n");
+    while (myTicketValueString != NULL) {
+        int value = atoi(myTicketValueString);
+        myTicket[idx] = value;
+        idx++;
+        myTicketValueString = strtok(NULL, ",\n");
+    }
+
+    long myTicketDeptartureValueProduct = 1;
+    for(int i = 0; i < NUM_TICKET_FIELDS; i++) {
+        if(strstr(fields[i]->name, "departure") != NULL) {
+            int fieldId = positionsInTicket[i];
+            myTicketDeptartureValueProduct *= myTicket[fieldId];
         }
     }
 
 
     printf("Puzzle 1 Answer: %i\n", scanningErrorRate);
+    printf("Puzzle 2 Answer: %li\n", myTicketDeptartureValueProduct);
 }
